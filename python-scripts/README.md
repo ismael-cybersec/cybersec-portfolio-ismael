7aa822e68c87340e36bcfc1715ad85214ff4ef25
ismael-cybersec <ismaelbaby2006@gmail.com>
2025-09-29 19:22:27 -0300

    Delete python-scripts directory

--- python-scripts/README.md
+++
@@ -1,3 +0,0 @@
-# Python Scripts
-
-Scripts utiles pour l'automatisation et l'analyse de logs.
--- python-scripts/log_analyzer.py
+++
@@ -1,4 +0,0 @@
-with open("log.txt") as f:
-    for line in f:
-        if "Failed" in line:
-            print(line)
